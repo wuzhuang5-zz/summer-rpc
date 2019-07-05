ac0c326321ea285f58f6aba3cbc23bb72270417c
wuzhuang <wuz0316@gmail.com>
2019-07-05 12:58:08 +0800

    '注册'

--- summer-registry/src/main/java/cn/wz/support/AbstractRegistry.java
+++ summer-registry/src/main/java/cn/wz/support/AbstractRegistry.java
@@ -1,7 +1,15 @@
 package cn.wz.support;
 
 import cn.wz.Registry;
+import cn.wz.rpc.URL;
 
 public abstract class AbstractRegistry implements Registry {
+    @Override
+    public void register(URL url) {
+        doRegister(url);
+    }
 
+    protected void doRegister(URL url) {
+
+    }
 }
