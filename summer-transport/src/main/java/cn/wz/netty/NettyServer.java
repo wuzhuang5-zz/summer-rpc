d481072e4e12d61cade1d1b3d09b7619f147374d
wuzhuang5 <wuz0316@gmail.com>
2019-10-08 15:49:42 +0800

    nettyserver

--- summer-transport/src/main/java/cn/wz/netty/NettyServer.java
+++ summer-transport/src/main/java/cn/wz/netty/NettyServer.java
@@ -2,13 +2,12 @@ package cn.wz.netty;
 
 public class NettyServer implements Channel {
 
-
     @Override
     public boolean open() {
-        return false;
+        return this.isAvailable();
     }
 
     public boolean isAvailable() {
-        return
+        return true;
     }
 }
