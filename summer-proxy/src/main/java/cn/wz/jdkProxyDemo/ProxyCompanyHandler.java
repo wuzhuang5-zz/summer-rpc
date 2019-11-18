49b3b91b692d8a89bd668facb731a275d1059a12
wuzhuang5 <wuz0316@gmail.com>
2019-11-18 12:33:30 +0800

    proxy

--- summer-proxy/src/main/java/cn/wz/jdkProxyDemo/ProxyCompanyHandler.java
+++ summer-proxy/src/main/java/cn/wz/jdkProxyDemo/ProxyCompanyHandler.java
@@ -4,10 +4,13 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 
 public class ProxyCompanyHandler implements InvocationHandler {
+
     private Object subject;
+
     public ProxyCompanyHandler(Object subject) {
         this.subject = subject;
     }
+
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
         System.out.println("中介公司找房子");
