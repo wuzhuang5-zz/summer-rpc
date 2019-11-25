299a95558d83235279100b44c382d8eaa33fff7f
wuzhuang5 <wuz0316@gmail.com>
2019-11-25 21:34:00 +0800

    zookeeperRegister

--- summer-register/src/main/java/cn/wz/Register.java
+++ summer-register/src/main/java/cn/wz/Register.java
@@ -1,9 +1,11 @@
 package cn.wz;
 
+import cn.wz.rpc.URL;
+
 /**
  * @Author: wz
  * @Date: 2019/11/18 12:27 下午
  */
 public interface Register {
-//    void register(URL url);
+    void register(URL url);
 }
--- summer-register/src/main/java/cn/wz/ZookeeperRegister.java
+++ summer-register/src/main/java/cn/wz/ZookeeperRegister.java
@@ -1,8 +1,19 @@
 package cn.wz;
 
+import cn.wz.common.exception.SummerFrameworkException;
+import cn.wz.rpc.URL;
+
 /**
  * @Author: wz
  * @Date: 2019/11/18 12:28 下午
  */
 public class ZookeeperRegister implements Register{
+
+    @Override
+    public void register(URL url) {
+        if (url == null) {
+            throw new SummerFrameworkException("register url is null");
+        }
+
+    }
 }
