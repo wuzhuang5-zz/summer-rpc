e5f67679a6e1ccc4f3d3673040f4a5f45b8c5620
wuzhuang5 <wuz0316@gmail.com>
2019-12-12 15:23:22 +0800

    序列化

--- summer-serializer/src/main/java/cn/wz/Serialization.java
+++ summer-serializer/src/main/java/cn/wz/Serialization.java
@@ -2,9 +2,25 @@ package cn.wz;
 
 import java.io.IOException;
 
+/**
+ * 序列化接口
+ */
 public interface Serialization {
-
+    /**
+     * 序列化
+     * @param object
+     * @return
+     * @throws IOException
+     */
     byte[] serialize(Object object) throws IOException;
 
+    /**
+     * 反序列化
+     * @param bytes
+     * @param clazz
+     * @param <T>
+     * @return
+     * @throws IOException
+     */
     <T> T deserialize(byte[] bytes, Class<T> clazz) throws IOException;
 }
--- summer-transport/src/main/java/cn/wz/netty/NettyChannelHandler.java
+++ summer-transport/src/main/java/cn/wz/netty/NettyChannelHandler.java
@@ -3,6 +3,9 @@ package cn.wz.netty;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+
 /**
  * @Author: wz
  */
@@ -24,6 +27,12 @@ public class NettyChannelHandler extends ChannelInboundHandlerAdapter {
      * @param msg
      */
     private void processMessage(ChannelHandlerContext ctx, NettyMessage msg) {
-
+        SocketAddress socketAddress = ctx.channel().remoteAddress();
+        String remoteIp = null;
+        if (socketAddress != null) {
+            remoteIp = ((InetSocketAddress) socketAddress).getAddress().getHostAddress();
+        }
+        Object result;
+        result =
     }
 }
