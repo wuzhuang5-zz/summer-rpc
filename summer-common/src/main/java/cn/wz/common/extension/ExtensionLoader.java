d40f6464184f12b91c77c6c66c77ec64d546525c
wuzhuang5 <wuz0316@gmail.com>
2019-12-17 10:57:39 +0800

    spi

--- summer-common/src/main/java/cn/wz/common/extension/ExtensionLoader.java
+++ summer-common/src/main/java/cn/wz/common/extension/ExtensionLoader.java
@@ -7,15 +7,34 @@ import java.util.concurrent.ConcurrentMap;
 
 /**
  * spi扩展类
+ *
  * @Author: wz
  */
 public class ExtensionLoader<T> {
-
     /**
      * 做本地缓存提升性能
      */
     private static ConcurrentMap<Class<?>, ExtensionLoader<?>> extensionLoaders = new ConcurrentHashMap<>();
 
+    private ConcurrentMap<String, Class<T>> extensionClasses = null;
+
+    public static final String PREFIX = "META-INF/services/";
+
+    private Class<T> type;
+
+    private ClassLoader classLoader;
+    /**
+     * 是否初始化
+     */
+    private volatile boolean init = false;
+
+    /**
+     * 获取本类
+     *
+     * @param type
+     * @param <T>
+     * @return
+     */
     public static <T> ExtensionLoader<T> getExtensionLoader(Class type) {
         //校验类型是否正确
         checkInterface(type);
@@ -26,13 +45,34 @@ public class ExtensionLoader<T> {
         return loader;
     }
 
-    private static synchronized <T> ExtensionLoader<T> initExtensionLoader(Class type) {
-        ExtensionLoader<T> loader = new ExtensionLoader<>();
-        return loader;
+    public T getExtension(String name) {
+        checkInit();
     }
 
 
+    private void checkInit() {
+        if (!init) {
+            loadExtensionClasses();
+        }
+    }
 
+    private synchronized void loadExtensionClasses() {
+        if (init) {
+            return;
+        }
+        extensionClasses = loadExtensionClasses(PREFIX);
+    }
+
+    private synchronized ConcurrentMap<String, Class<T>> loadExtensionClasses(String prefix) {
+        String fullName = prefix + type.getName();
+
+    }
+
+    private static synchronized <T> ExtensionLoader<T> initExtensionLoader(Class type) {
+        ExtensionLoader<T> loader = new ExtensionLoader<>();
+        extensionLoaders.putIfAbsent(type, loader);
+        return loader;
+    }
 
     private static void checkInterface(Class clazz) {
         if (clazz == null) {
