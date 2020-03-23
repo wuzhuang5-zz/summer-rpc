4ccbaff9663e5e1ce29fc68bc35748afaba12d99
wuzhuang5 <wuz0316@gmail.com>
2020-03-23 21:15:07 +0800

    常量设置

--- summer-common/src/main/java/cn/wz/common/util/NetUtil.java
+++ summer-common/src/main/java/cn/wz/common/util/NetUtil.java
@@ -1,5 +1,8 @@
 package cn.wz.common.util;
 
+/**
+ * 网络工具类
+ */
 public class NetUtil {
     public static final String LOCALHOST = "127.0.0.1";
 }
--- summer-common/src/main/java/cn/wz/common/util/SummerConstans.java
+++ summer-common/src/main/java/cn/wz/common/util/SummerConstans.java
@@ -1,8 +1,12 @@
 package cn.wz.common.util;
 
+/**
+ * summer 常量
+ */
 public class SummerConstans {
     public static final int DEFAULT_INT_VALUE = 0;
     public static final String PROTOCOL_SEPARATOR = "://";
     public static final String PATH_SEPARATOR = "/";
     public static final String SUMMER = "summer";
+    byte[] bytes = new byte[1024];
 }
